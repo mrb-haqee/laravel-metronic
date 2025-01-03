857ff294581554e06e5e0240af3d9b41953af2cc
mrb-haqee <raflibahaqi890@gmail.com>
2025-01-03 14:35:42 +0800

    code commit

---
+++ fiveserver.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    php: "C:\\xampp\\php\\php.exe", // Path PHP Anda
+    proxy: "http://127.0.0.1:8000", // Laravel default server
+    watch: ["resources/views/**/*.blade.php", "public/**/*.*"], // File yang diawasi
+};
